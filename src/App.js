4db15d390ea0966c7adc988915211ad574c505de
hnz989 <zhenghualin2008@gmail.com>
2022-08-06 09:56:08 -0400

    Pre Navbar

--- src/App.js
+++ src/App.js
@@ -6,12 +6,14 @@ import Error from './pages/Error'
 
 function App() {
   return <BrowserRouter>
+    <nav>Our Navbar</nav>
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="about" element={<About />} />
       <Route path="prodcuts" element={<Products />} />
       <Route path="*" element={<Error />} />
     </Routes>
+    <footer>Our Footer</footer>
   </BrowserRouter>;
 }
 
