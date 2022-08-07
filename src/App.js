8bc3368546cf305e5eb269fea366736c7d21dda1
hnz989 <zhenghualin2008@gmail.com>
2022-08-07 00:43:38 -0400

    Products Page Refactor

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import SingleProduct from "./pages/SingleProduct";
 import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
 import ProtectedRoute from "./pages/ProtectedRoute";
+import SharedProductLayout from "./pages/SharedProductLayout";
 
 function App() {
     const [user, setUser] = useState(null);
@@ -18,8 +19,10 @@ function App() {
             <Route path="/" element={<SharedLayout/>}>
                 <Route index element={<Home/>}/>
                 <Route path="about" element={<About/>}/>
-                <Route path="products" element={<Products/>}/>
-                <Route path="products/:productId" element={<SingleProduct/>}/>
+                <Route path="products" element={<SharedProductLayout/>}>
+                    <Route index element={<Products/>}/>
+                    <Route path=":productId" element={<SingleProduct/>}/>
+                </Route>
                 <Route path='login' element={<Login setUser={setUser}></Login>}/>
                 <Route path='dashboard' element={
                     <ProtectedRoute user={user}>
@@ -28,9 +31,6 @@ function App() {
                 }
                 />
                 <Route path="*" element={<Error/>}/>
-            </Route>
-            <Route path="dashboard" element={<div>Our Dashboard</div>}>
-                <Route path="stats" element={<div>Stats</div>}/>
             </Route>
         </Routes>
         <footer>Our Footer</footer>
