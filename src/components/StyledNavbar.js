874bab243a181cb8a435d6191d932ed4e81e94b6
hnz989 <zhenghualin2008@gmail.com>
2022-08-06 23:38:30 -0400

    useNavigate() and useState()

--- src/components/StyledNavbar.js
+++ src/components/StyledNavbar.js
@@ -6,6 +6,7 @@ const StyledNavbar = () => {
             <NavLink to='/' className={({isActive}) => isActive ? 'link active' : 'link'}>Home</NavLink>
             <NavLink to='/about' className={({isActive}) => isActive ? 'link active' : 'link'}>About</NavLink>
             <NavLink to='/products' className={({isActive}) => isActive ? 'link active' : 'link'}>Products</NavLink>
+            <NavLink to='/login' className={({isActive}) => isActive ? 'link active' : 'link'}>Login</NavLink>
         </nav>
     )
 
